acd4139ec62d68f00486e8c21d4f7f6658e084d9
sunder3344 <sunder3344@sina.com>
2016-10-21 10:24:30 +0800

    add cocos framework

---
+++ frameworks/runtime-src/proj.android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\bin\AndroidManifest.xml : \
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\AndroidManifest.xml \
---
+++ frameworks/runtime-src/proj.android/bin/cocos-debug-unaligned.apk.d
@@ -0,0 +1,9 @@
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\bin\cocos-debug-unaligned.apk : \
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\bin\cocos.ap_ \
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\bin\classes.dex \
+D:\Platform\php\workspace\cocos\frameworks\cocos2d-x\cocos\platform\android\java\bin\classes.jar \
+D:\Platform\php\workspace\cocos\frameworks\cocos2d-x\cocos\platform\android\java\libs\com.android.vending.expansion.zipfile.jar \
+D:\Platform\php\workspace\cocos\frameworks\cocos2d-x\cocos\platform\android\java\libs\httpclient-4.4.1.1.jar \
+D:\Platform\php\workspace\cocos\frameworks\cocos2d-x\cocos\platform\android\java\libs\android-async-http-1.4.9.jar \
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\libs\armeabi\gdbserver \
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\libs\armeabi\libcocos2djs.so \
