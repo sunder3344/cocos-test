0adda6a378aa0b50bff87dca7d503dc635ad853f
sunder3344 <sunder3344@sina.com>
2016-10-21 10:34:45 +0800

    add cocos framework

---
+++ frameworks/runtime-src/proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\gen\org\cocos2dx\cocos\R.java \
+ : D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\res\drawable-hdpi\icon.png \
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\res\drawable-ldpi\icon.png \
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\res\drawable-mdpi\icon.png \
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\res\values\strings.xml \
+D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\bin\AndroidManifest.xml \
