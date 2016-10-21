7c35bde38913a65959d0b7434d4c52c5555a72ae
sunder3344 <sunder3344@sina.com>
2016-10-21 17:38:31 +0800

    delete framework file

--- frameworks/runtime-src/proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\bin\AndroidManifest.xml : \
-D:\Platform\php\workspace\cocos\frameworks\runtime-src\proj.android\AndroidManifest.xml \
